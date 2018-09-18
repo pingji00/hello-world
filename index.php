91700a7d88b71df3f241824f34092270e4b419f8
pingji00 <pingji@me.com>
2018-09-18 18:49:09 -0400

    adding css and images and homepage

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<h1>Hello World</h1>
+	<img src="images/wc_winter.jpg" alt="Wellesley Winter">
+</body>
+</html>
