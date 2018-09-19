14f6155fa276c74c62a1309cb61caafbbbcc6bb2
pingji00 <pingji@me.com>
2018-09-18 20:52:32 -0400

    content change on mainpage.

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-	<h1>Hello World</h1>
+	<h1>Hello World!!</h1>
+	<h2>test</h2>
 	<img src="images/wc_winter.jpg" alt="Wellesley Winter">
 </body>
 </html>
